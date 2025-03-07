18ffdb2940b644c4c5b9eaa21e29860abf97e13f
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2025-03-07 11:14:11 +0800

    setup the basic CRUD operation using RESTful restrains, will be adding more complex endpoints later on.

---
+++ src/DevHabit.Api/DTOs/Habits/HabitMappings.cs
@@ -0,0 +1,107 @@
+﻿using DevHabit.Api.Entities;
+
+namespace DevHabit.Api.DTOs.Habits;
+
+internal static class HabitMappings
+{
+    public static HabitDto ToDto(this Habit habit)
+    {
+        return new HabitDto
+        {
+            Id = habit.Id,
+            Name = habit.Name,
+            Description = habit.Description,
+            Type = habit.Type,
+            Frequency = new FrequencyDto
+            {
+                Type = habit.Frequency.Type,
+                TimesPerPeriod = habit.Frequency.TimesPerPeriod
+            },
+            Target = new TargetDto
+            {
+                Value = habit.Target.Value,
+                Unit = habit.Target.Unit
+            },
+            Status = habit.Status,
+            IsArchived = habit.IsArchived,
+            EndDate = habit.EndDate,
+            Milestone = habit.Milestone == null
+                ? null
+                : new MilestoneDto
+                {
+                    Target = habit.Milestone.Target,
+                    Current = habit.Milestone.Current
+                },
+            CreatedAtUtc = habit.CreatedAtUtc,
+            UpdatedAtUtc = habit.UpdatedAtUtc,
+            LastCompletedAtUtc = habit.LastCompletedAtUtc
+        };
+    }
+
+    public static Habit ToEntity(this CreateHabitDto dto)
+    {
+        Habit habit = new()
+        {
+            Id = $"h_{Guid.CreateVersion7()}",
+            Name = dto.Name,
+            Description = dto.Description,
+            Type = dto.Type,
+            Frequency = new Frequency
+            {
+                Type = dto.Frequency.Type,
+                TimesPerPeriod = dto.Frequency.TimesPerPeriod
+            },
+            Target = new Target
+            {
+                Value = dto.Target.Value,
+                Unit = dto.Target.Unit
+            },
+            Status = HabitStatus.Ongoing,
+            IsArchived = false,
+            EndDate = dto.EndDate,
+            Milestone = dto.Milestone is not null
+                ? new Milestone
+                {
+                    Target = dto.Milestone.Target,
+                    Current = 0 // Initialize current progress to 0
+                }
+                : null,
+            CreatedAtUtc = DateTime.UtcNow
+        };
+
+        return habit;
+    }
+
+    public static void UpdateFromDto(this Habit habit, UpdateHabitDto dto)
+    {
+        // Update basic properties
+        habit.Name = dto.Name;
+        habit.Description = dto.Description;
+        habit.Type = dto.Type;
+        habit.EndDate = dto.EndDate;
+
+        // Update frequency (assuming it's immutable, create new instance)
+        habit.Frequency = new Frequency
+        {
+            Type = dto.Frequency.Type,
+            TimesPerPeriod = dto.Frequency.TimesPerPeriod
+        };
+
+        // Update target
+        habit.Target = new Target
+        {
+            Value = dto.Target.Value,
+            Unit = dto.Target.Unit
+        };
+
+        // Update milestone if provided
+        if (dto.Milestone != null)
+        {
+            habit.Milestone ??= new Milestone(); // Create new if doesn't exist
+            habit.Milestone.Target = dto.Milestone.Target;
+            // Note: We don't update Milestone.Current from DTO to preserve progress
+        }
+
+        habit.UpdatedAtUtc = DateTime.UtcNow;
+    }
+}
--- src/DevHabit.Api/Database/ApplicationDbContext.cs
+++ src/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Api.Database;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : DbContext(options)
 {
+    public DbSet<Habit> Habits { get; set; }
+    public DbSet<Tag> Tags { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Application);
