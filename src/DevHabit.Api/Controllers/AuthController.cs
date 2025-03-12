030375a6de3506b4e4e7f7854e3ad9c310c64031
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2025-03-12 18:11:01 +0800

    created refreshtoken basic rotation structure

--- src/DevHabit.Api/Controllers/AuthController.cs
+++ src/DevHabit.Api/Controllers/AuthController.cs
@@ -3,12 +3,14 @@ using DevHabit.Api.DTOs.Auth;
 using DevHabit.Api.DTOs.Users;
 using DevHabit.Api.Entities;
 using DevHabit.Api.Services;
+using DevHabit.Api.Settings;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Options;
 
 namespace DevHabit.Api.Controllers;
 
@@ -19,9 +21,12 @@ public sealed class AuthController(
     UserManager<IdentityUser> userManager,
     ApplicationIdentityDbContext identityDbContext,
     ApplicationDbContext applicationDbContext,
-    TokenProvider tokenProvider)
+    TokenProvider tokenProvider,
+    IOptions<JwtAuthOptions> options)
     : ControllerBase
 {
+    private readonly JwtAuthOptions _jwtAuthOptions = options.Value;
+
     [HttpPost("register")]
     public async Task<ActionResult<AccessTokensDto>> Register(RegisterUserDto registerUserDto)
     {
@@ -61,13 +66,27 @@ public sealed class AuthController(
 
         await applicationDbContext.SaveChangesAsync();
 
-        await transaction.CommitAsync();
-
+        // Create access tokens
         var tokenRequest = new TokenRequest(
             identityUser.Id,
             identityUser.Email);
 
         AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+
+        // Create refresh token
+        var refreshToken = new RefreshToken
+        {
+            Id = Guid.CreateVersion7(),
+            UserId = identityUser.Id,
+            Token = accessTokens.RefreshToken,
+            ExpiresAtUtc = DateTime.UtcNow.AddDays(_jwtAuthOptions.RefreshTokenExpirationDays)
+        };
+        identityDbContext.RefreshTokens.Add(refreshToken);
+
+        await identityDbContext.SaveChangesAsync();
+
+        // Commit transactions
+        await transaction.CommitAsync();
 
         return Ok(accessTokens);
     }
@@ -87,6 +106,49 @@ public sealed class AuthController(
 
         AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
 
+        // Create refresh token
+        var refreshToken = new RefreshToken
+        {
+            Id = Guid.CreateVersion7(),
+            UserId = identityUser.Id,
+            Token = accessTokens.RefreshToken,
+            ExpiresAtUtc = DateTime.UtcNow.AddDays(_jwtAuthOptions.RefreshTokenExpirationDays)
+        };
+        identityDbContext.RefreshTokens.Add(refreshToken);
+
+        await identityDbContext.SaveChangesAsync();
+
+        return Ok(accessTokens);
+    }
+
+    [HttpPost("refresh")]
+    public async Task<ActionResult<AccessTokensDto>> Refresh(RefreshTokenDto refreshTokenDto)
+    {
+        RefreshToken? refreshToken = await identityDbContext.RefreshTokens
+            .Include(rt => rt.User)
+            .FirstOrDefaultAsync(rt => rt.Token == refreshTokenDto.RefreshToken);
+
+        if (refreshToken is null)
+        {
+            return Unauthorized();
+        }
+
+        if (refreshToken.ExpiresAtUtc < DateTime.UtcNow)
+        {
+            return Unauthorized();
+        }
+
+        // Create access tokens
+        var tokenRequest = new TokenRequest(
+            refreshToken.UserId,
+            refreshToken.User.Email!);
+        AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+
+        // Rotate refresh token
+        refreshToken.Token = accessTokens.RefreshToken;
+        refreshToken.ExpiresAtUtc = DateTime.UtcNow.AddDays(_jwtAuthOptions.RefreshTokenExpirationDays);
+
+        await identityDbContext.SaveChangesAsync();
         return Ok(accessTokens);
     }
 }
---
+++ src/DevHabit.Api/DTOs/Auth/RefreshTokenDto.cs
@@ -0,0 +1,6 @@
+﻿namespace DevHabit.Api.DTOs.Auth;
+
+public sealed record class RefreshTokenDto
+{
+    public required string RefreshToken { get; init; }
+}
