000332db5fde82a3053dac5c8862d87eb9220d3a
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2025-03-17 17:19:55 +0800

    implementing example of refit, for external api endpoints.

--- src/DevHabit.Api/DependencyInjection.cs
+++ src/DevHabit.Api/DependencyInjection.cs
@@ -25,6 +25,7 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using Quartz;
+using Refit;
 
 namespace DevHabit.Api;
 
@@ -155,6 +156,8 @@ public static class DependencyInjection
 
         builder.Services.AddScoped<GitHubAccessTokenService>();
         builder.Services.AddTransient<GitHubService>();
+        builder.Services.AddTransient<RefitGitHubService>();
+
         builder.Services
             .AddHttpClient("github")
             .ConfigureHttpClient(client =>
@@ -167,6 +170,13 @@ public static class DependencyInjection
                 client.DefaultRequestHeaders
                     .Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
             });
+
+        builder.Services
+            .AddRefitClient<IGitHubApi>(new RefitSettings
+            {
+                ContentSerializer = new NewtonsoftJsonContentSerializer()
+            })
+            .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://api.github.com"));
 
         builder.Services.Configure<EncryptionOptions>(
             builder.Configuration.GetSection(EncryptionOptions.SectionName));
