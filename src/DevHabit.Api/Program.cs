0362c448f5428e390cc444bd713ad61427c7539b
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2025-03-20 17:51:58 +0800

    adding a external scalar for documentation.

--- src/DevHabit.Api/Program.cs
+++ src/DevHabit.Api/Program.cs
@@ -2,6 +2,7 @@ using DevHabit.Api;
 using DevHabit.Api.Extensions;
 using DevHabit.Api.Middleware;
 using DevHabit.Api.Settings;
+using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,11 @@ if (app.Environment.IsDevelopment())
     //app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.MapScalarApiReference(options =>
+    {
+        options.WithOpenApiRoutePattern("/swagger/1.0/swagger.json");
+    });
 
     await app.ApplyMigrationsAsync();
 
